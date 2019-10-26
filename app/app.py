6af13deb8929da52186b8026cdcd65b784b1e011
lynoska <lynoskagarcia@mail.adelphi.edu>
2019-10-26 02:28:01 -0400

    incorporated the mongo database

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 import os
+import config
 
 from flask import Flask, render_template
 from . import settings, views, models
-from .extensions import mongo
+from flask_pymongo import PyMongo
+# from .extensions import mongo
+
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,15 +13,20 @@ def create_app(config_object=settings):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_object)
+    app.config["MONGO_URI"] = config.MONGO_URI
+    # app.config['MONGO_DBNAME'] = config.MONGO_DBNAME
+    mongo = PyMongo(app)
+    db = mongo.db
 
-    # register_extensions(app)
+    # register_extensions(app, mongo)
     register_blueprints(app)
     register_errorhandlers(app)
     return app
 
-def register_extensions(app):
+def register_extensions(app, mongo):
     """Register Flask extensions."""
     mongo.db.init_app(app)
+    # client = PyMongo.MongoClient(confid.MONGO_URI)
 
     with app.app_context():
         mongo.db.create_all()
