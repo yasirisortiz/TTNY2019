c7582b7b8cdb7e74238e8b4305c350de744bb06b
lynoska <lynoskagarcia@mail.adelphi.edu>
2019-10-26 17:11:10 -0400

    corrected calling to Users model; set up for future use

--- app/views/home.py
+++ app/views/home.py
@@ -1,16 +1,12 @@
 from flask import Blueprint, render_template
-from ..extensions import mongo
+from ..models import models
 import os
-
 
 blueprint = Blueprint("home", __name__)
 
 # index route
 @blueprint.route("/")
 def index():
-	print(mongo.db)
-	user_collection = mongo.db.TTNY
-	user_collection.insert_one({"name" : "Test Usersssssssssssssss"})
-	user_collection.insert_one({"name" : "ERSGGS"})
+	users = models.Users.objects.all()
 
 	return render_template("home/index.html")
