b005a49ba2fe225f940eb88a7b17167f425055d0
lynoska <lynoskagarcia@mail.adelphi.edu>
2019-10-26 17:13:06 -0400

    init setup and models created

---
+++ app/models/models.py
@@ -0,0 +1,38 @@
+# from app.extensions import mongo
+from flask_mongoengine import MongoEngine
+from mongoengine import Document, EmbeddedDocument, StringField, EmailField, ListField, BooleanField, EmbeddedDocumentField, ImageField, IntField
+
+
+db = MongoEngine()
+
+
+class Organization(db.EmbeddedDocument):
+    name = StringField(max_length=300)
+    donation_type = StringField(max_length=100)
+    location = EmbeddedDocumentField("Location")
+
+
+class Statistics(db.EmbeddedDocument):
+    donated = IntField()
+    received = IntField()
+    delivered = IntField()
+
+
+class Location(db.EmbeddedDocument):
+    street = StringField(max_length=100)
+    city = StringField(max_length=50)
+    state = StringField(max_length=2)
+    zip = IntField()
+
+
+class Users(db.Document):
+    username = StringField(max_length=50, required=True, unique=True)
+    fname = StringField(max_length=30, required=True)
+    lname = StringField(max_length=30, required=True)
+    email = EmailField(max_length=100, required=True, unique=True)
+    volunteer = BooleanField(required=True)
+    description = StringField(max_length=300)
+    languages = ListField(StringField(), default=list)
+    photo = ImageField()
+    organization = EmbeddedDocumentField("Organization")
+    stats = EmbeddedDocumentField("Statistics")
